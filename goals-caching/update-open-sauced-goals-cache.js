9eec6fa2d17742f514c7dde5764bb79647876ce2
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2021-07-31 16:07:11 -0700

    working stars.json

--- goals-caching/update-open-sauced-goals-cache.js
+++ goals-caching/update-open-sauced-goals-cache.js
@@ -1,5 +1,4 @@
-import {Octokit} from "@octokit/rest"
-import { request } from "@octokit/request";
+import {Octokit} from "octokit"
 import fs from "fs";
 
 const octokit = new Octokit({
@@ -30,7 +29,7 @@ async function getRepoGoals(issues) {
       // all goal issues follow the "owner/repo" format 
       let [owner, name] = issue.title.split("/");
   
-      const {data} = await octokit.repos.get({
+      const {data} = await octokit.rest.repos.get({
         owner: owner,
         repo: name,
       })
