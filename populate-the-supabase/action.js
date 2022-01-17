beb536adedb16c34eb34e7f8cf54a8bd7b9317a0
brian <ilikerobot@gmail.com>
2022-01-16 17:50:34 -0800

    update table to collect goal repo statsr

--- populate-the-supabase/action.js
+++ populate-the-supabase/action.js
@@ -36,9 +36,10 @@ async function run() {
         return {data: {content: []}}
       });
 
-      // convert from base64 to parseable JSOON 
+      // convert from base64 to parseable JSON
       const content = Buffer.from(data.content, "base64").toString()
       const parsedData = JSON.parse(content)
+      const starsData = data.content.length > 0
 
       // update data with repo id
       for (const item of parsedData) {
@@ -70,17 +71,19 @@ async function run() {
         })
       }
 
-     // send parsedData to stars table
-     await supabase.from('stars').upsert(parsedData  )
-
       console.log(`ADDED STARS FROM: ${repository.html_url}`)
 
       // send parsedData to supabase
-      supabase.from('users')
-      .upsert({id: repository.owner.id, login: repository.owner.login})
+     await supabase.from('users')
+      .upsert({id: repository.owner.id, stars_data: starsData,
+        open_issues: repository.open_issues, private: repository.private}, {
+          onConflict:
+          'id'
+        })
+
+      console.log(`ADDED USER FROM: ${repository.owner.login}`)
 
     } catch (err) {
-      console.log(`ERROR: ${err}`)
       console.log(`SKIPPED: ${repository.html_url}`)
       return
     }
