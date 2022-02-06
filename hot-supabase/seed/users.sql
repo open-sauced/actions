341c6fa87012ebf8ebae756a2799f2f1cf7c842a
bdougie <bdougie@users.noreply.github.com>
2022-02-06 16:49:28 +0000

    chore: seed supabase data [skip ci]

--- hot-supabase/seed/users.sql
+++ hot-supabase/seed/users.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: users; Type: TABLE DATA; Schema: public; Timestamp: 2022-02-06T14:43:22.447Z
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Timestamp: 2022-02-06T16:49:26.933Z
 -- COPY users(id, login, private, open_issues, stars_data) FROM 'users.csv' DELIMITER ',' CSV HEADER;
 --
 
--- hot-supabase/seed/votes.sql
+++ hot-supabase/seed/votes.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: votes; Type: TABLE DATA; Schema: public; Timestamp: 2022-02-06T14:43:22.434Z
+-- Data for Name: votes; Type: TABLE DATA; Schema: public; Timestamp: 2022-02-06T16:49:26.865Z
 -- COPY votes(voteid, created_at, github_user_id, repo_name, vote_code) FROM 'votes.csv' DELIMITER ',' CSV HEADER;
 --
 
