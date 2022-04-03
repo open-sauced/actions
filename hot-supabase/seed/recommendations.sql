d4c5cb5b7930cf0f26460a69032e7471f4fea4bb
bdougie <bdougie@users.noreply.github.com>
2022-04-03 12:51:02 +0000

    chore: seed supabase data [skip ci]

--- hot-supabase/seed/recommendations.sql
+++ hot-supabase/seed/recommendations.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: recommendations; Type: TABLE DATA; Schema: public; Timestamp: 2022-04-03T10:44:03.020Z
+-- Data for Name: recommendations; Type: TABLE DATA; Schema: public; Timestamp: 2022-04-03T12:51:01.809Z
 -- COPY recommendations(id, stars, issues, total_stars, votes, avg_recency_score, repo_name, description, contributors, created_at) FROM 'recommendations.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
