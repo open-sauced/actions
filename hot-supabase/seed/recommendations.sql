dec8831fa8296d19372235e34ebbe24e62b4fb4d
bdougie <bdougie@users.noreply.github.com>
2022-05-04 06:48:02 +0000

    chore: seed supabase data [skip ci]

--- hot-supabase/seed/recommendations.sql
+++ hot-supabase/seed/recommendations.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: recommendations; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-04T04:51:36.532Z
+-- Data for Name: recommendations; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-04T06:48:01.282Z
 -- COPY recommendations(id, stars, issues, total_stars, votes, avg_recency_score, repo_name, description, contributors, created_at) FROM 'recommendations.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
@@ -342,8 +342,8 @@ INSERT INTO recommendations(id, stars, issues, total_stars, votes, avg_recency_s
 (10379106, 1785, 40, 1, 0, 26, 'browserify/watchify', 'watch mode for browserify builds', ARRAY['substack', 'zertosh'], '2022-02-24T21:06:05.080468+00:00'),
 (10395188, 6481, 287, 1, 0, 21, 'exercism/exercism', 'Crowd-sourced code mentorship. Practice having thoughtful conversations about code.', ARRAY['iHiD', 'exercism-bot'], '2022-02-24T21:06:05.080468+00:00'),
 (10489882, 2436, 1, 1, 0, 8, 'nginx-boilerplate/nginx-boilerplate', 'Awesome Nginx configuration template', ARRAY['Umkus', 'agarzon'], '2022-02-24T21:06:05.080468+00:00'),
-(10530598, 784, 0, 1, 0, 1, 'greggigon/my-personal-kanban', 'This is a one page HTML/JavaScript application for people who would like to use simple and basic Kanban board for their personal stuff', ARRAY['greggigon', 'greggigon-dlg'], '2022-02-24T21:06:05.080468+00:00'),
 (10567197, 22166, 60, 1, 0, 10, 'dokku/dokku', 'A docker-powered PaaS that helps you build and manage the lifecycle of applications', ARRAY['josegonzalez', 'michaelshobbs'], '2022-02-24T21:06:05.080468+00:00'),
+(10530598, 784, 0, 1, 0, 1, 'greggigon/my-personal-kanban', 'This is a one page HTML/JavaScript application for people who would like to use simple and basic Kanban board for their personal stuff', ARRAY['greggigon', 'greggigon-dlg'], '2022-02-24T21:06:05.080468+00:00'),
 (10581067, 60, 0, 1, 0, 12, 'kerryb/goos-ruby', 'The worked example from Growing Object-Oriented Software, Guided By Tests, ported to Ruby', ARRAY['kerryb'], '2022-02-24T21:06:05.080468+00:00'),
 (10639547, 822, 8, 1, 0, 21, 'ResponsiveBP/Responsive', ':iphone: A super lightweight HTML, Sass, CSS, and JavaScript framework for building responsive websites', ARRAY['JimBobSquarePants', 'dampee'], '2022-02-24T21:06:05.080468+00:00'),
 (10651420, 4536, 73, 1, 0, 16, 'tmate-io/tmate', 'Instant Terminal Sharing', ARRAY['nicm', 'ThomasAdam'], '2022-02-24T21:06:05.080468+00:00'),
@@ -540,8 +540,8 @@ INSERT INTO recommendations(id, stars, issues, total_stars, votes, avg_recency_s
 (21978103, 447, 0, 1, 0, 6, 'scottaohara/accessible_components', 'Listing of accessible components & patterns', ARRAY['scottaohara', 'kethinov'], '2022-02-24T21:06:05.080468+00:00'),
 (22091630, 255, 37, 1, 0, 8, 'RangeNetworks/OpenBTS-UMTS', '3G UMTS Data Radio Access Network Node', ARRAY['ttsou', 'iedemam'], '2022-02-24T21:06:05.080468+00:00'),
 (22119721, 38201, 14, 2, 0, 5.5, 'k88hudson/git-flight-rules', 'Flight rules for git', ARRAY['RichardLitt', 'simaki'], '2022-02-24T21:06:05.080468+00:00'),
-(22152430, 720, 11, 1, 0, 1, 'mrflix/dimensions', 'A Chrome extension for measuring screen dimensions', ARRAY['mrflix', 'arnaugiralt'], '2022-02-24T21:06:05.080468+00:00'),
 (22242868, 244, 3, 1, 0, 21, 'edgarjs/alfred-github-repos', 'Alfred workflow to easily open Github repositories', ARRAY['edgarjs', 'ileodo'], '2022-02-24T21:06:05.080468+00:00'),
+(22152430, 720, 11, 1, 0, 1, 'mrflix/dimensions', 'A Chrome extension for measuring screen dimensions', ARRAY['mrflix', 'arnaugiralt'], '2022-02-24T21:06:05.080468+00:00'),
 (22258112, 1558, 34, 1, 0, 21, 'quantifiedcode/python-anti-patterns', 'An open collection of Python anti-patterns and worst practices.', ARRAY['kaycebasques', 'adewes'], '2022-02-24T21:06:05.080468+00:00'),
 (22336000, 852, 35, 1, 0, 22, 'Pragmatists/JUnitParams', 'Parameterised tests that don''t suck', ARRAY['plipinski', 'woprzech'], '2022-02-24T21:06:05.080468+00:00'),
 (22386765, 2363, 162, 1, 0, 23, 'NUKnightLab/TimelineJS3', 'TimelineJS v3: A Storytelling Timeline built in JavaScript.  http://timeline.knightlab.com', ARRAY['JoeGermuska', 'zachwise'], '2022-02-24T21:06:05.080468+00:00'),
@@ -3302,10 +3302,10 @@ INSERT INTO recommendations(id, stars, issues, total_stars, votes, avg_recency_s
 (373327367, 30, 0, 1, 0, 12, 'rx-mind/ngrx-plugins', 'Reactive Angular Plugins', ARRAY['markostanimirovic'], '2022-02-24T21:06:05.080468+00:00'),
 (373396052, 44, 2, 1, 0, 29, 'ri7nz/rescript-chakra', '⚡️⚛️ ReScript bindings for @chakra-ui/react', ARRAY['ri7nz', 'semantic-release-bot'], '2022-02-24T21:06:05.080468+00:00'),
 (373495126, 335, 1, 1, 0, 16, 'thevinter/styli.sh', 'A CLI tool for easy wallpaper management and image fetching', ARRAY['thevinter', 'ma3yta'], '2022-02-24T21:06:05.080468+00:00'),
+(373640770, 4, 6, 2, 0, 24.5, 'hughrawlinson/battlesnake', 'Battlesnake JS Bindings and Types', ARRAY['hughrawlinson'], '2022-02-24T21:06:05.080468+00:00'),
 (373681744, 1, 0, 1, 0, 29, 'muhammaddzaky/project-kalian', 'Hello', ARRAY['muhammaddzaky'], '2022-02-24T21:06:05.080468+00:00'),
 (373693162, 5, 0, 1, 0, 26, 'imamdigmi/BicaraDANA', null, ARRAY['imamdigmi'], '2022-02-24T21:06:05.080468+00:00'),
 (373919310, 10, 0, 1, 0, 21, 'ionic-elements/crypto-starter-ionic', 'Crypto app with market data, graphs and latest news.', ARRAY['fmendoza'], '2022-02-24T21:06:05.080468+00:00'),
-(373640770, 4, 6, 2, 0, 24.5, 'hughrawlinson/battlesnake', 'Battlesnake JS Bindings and Types', ARRAY['hughrawlinson'], '2022-02-24T21:06:05.080468+00:00'),
 (373943090, 7, 0, 1, 0, 2, 'tspannhw/SpeakerProfile', 'My speaker profile for events and conferences based on codepo8/presenter-terms', ARRAY['tspannhw'], '2022-02-24T21:06:05.080468+00:00'),
 (374018112, 1006, 0, 1, 0, 26, 'sadanandpai/frontend-learning-kit', 'Frontend tech guide and collection of highly recommended materials', ARRAY['sadanandpai', 'dth99'], '2022-02-24T21:06:05.080468+00:00'),
 (374067908, 95, 0, 1, 0, 11, 'theredditbandit/awesome-cybersec', 'A collection of awesome platforms, blogs, documents, books, resources and cool stuff about security ', ARRAY['theredditbandit'], '2022-02-24T21:06:05.080468+00:00'),
--- hot-supabase/seed/votes.sql
+++ hot-supabase/seed/votes.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: votes; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-04T04:51:36.213Z
+-- Data for Name: votes; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-04T06:48:00.871Z
 -- COPY votes(id, github_user_id, repo_name, code, created_at) FROM 'votes.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
