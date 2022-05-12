19e654651ebb51be0fb68bc34b4dc199121e4e5b
bdougie <bdougie@users.noreply.github.com>
2022-05-12 03:07:18 +0000

    chore: seed supabase data [skip ci]

--- hot-supabase/seed/votes.sql
+++ hot-supabase/seed/votes.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: votes; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-12T01:03:51.564Z
+-- Data for Name: votes; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-12T03:07:17.390Z
 -- COPY votes(id, github_user_id, repo_name, code, created_at) FROM 'votes.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
