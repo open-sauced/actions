6294c30c78b20fac76159defdef79c85393d373e
bdougie <bdougie@users.noreply.github.com>
2022-05-19 20:44:48 +0000

    chore: seed supabase data [skip ci]

--- hot-supabase/seed/recommendations.sql
+++ hot-supabase/seed/recommendations.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: recommendations; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-19T18:49:39.817Z
+-- Data for Name: recommendations; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-19T20:44:47.027Z
 -- COPY recommendations(id, stars, issues, total_stars, votes, avg_recency_score, repo_name, description, contributors, created_at) FROM 'recommendations.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
@@ -2228,8 +2228,8 @@ INSERT INTO recommendations(id, stars, issues, total_stars, votes, avg_recency_s
 (221512407, 3471, 20, 1, 0, 11, 'neovim/nvim-lspconfig', 'Quickstart configurations for the Nvim LSP client', ARRAY['mjlbach', 'h-michael'], '2022-02-24T21:06:05.080468+00:00'),
 (221631156, 1163, 21, 1, 0, 1, 'trekhleb/machine-learning-experiments', '🤖 Interactive Machine Learning experiments: 🏋️models training + 🎨models demo', ARRAY['trekhleb'], '2022-02-24T21:06:05.080468+00:00'),
 (221981891, 31667, 447, 3, 0, 11.333333333333334, 'microsoft/playwright', 'Playwright is a framework for Web Testing and Automation. It allows testing Chromium, Firefox and WebKit with a single API. ', ARRAY['pavelfeldman', 'dgozman'], '2022-02-24T21:06:05.080468+00:00'),
-(222079167, 272, 34, 1, 0, 11, 'shankarpandala/lazypredict', 'Lazy Predict help build a lot of basic models without much code and helps understand which models works better without any parameter tuning', ARRAY['shankarpandala', 'brendalf'], '2022-02-24T21:06:05.080468+00:00'),
 (222007330, 382, 43, 1, 0, 4, 'microsoft/rnx-kit', 'Tools which help developers build, deliver, and maintain React Native apps and libraries.', ARRAY['tido64', 'rnbot'], '2022-02-24T21:06:05.080468+00:00'),
+(222079167, 272, 34, 1, 0, 11, 'shankarpandala/lazypredict', 'Lazy Predict help build a lot of basic models without much code and helps understand which models works better without any parameter tuning', ARRAY['shankarpandala', 'brendalf'], '2022-02-24T21:06:05.080468+00:00'),
 (222090175, 94, 5, 1, 0, 21, 'jovton/USB-Storage-on-WSL2', 'This article describes how you can access your USB storage device from Microsoft''s Windows Subsystem for Linux 2 (WSL2)', ARRAY['jovton', 'tskoda23'], '2022-02-24T21:06:05.080468+00:00'),
 (222103697, 337, 22, 1, 0, 1, 'simbathesailor/use-what-changed', 'A React hook and an easy to use babel-pugin to debug various React official hooks', ARRAY['simbathesailor', 'LiuL0703'], '2022-02-24T21:06:05.080468+00:00'),
 (222258954, 3609, 30, 1, 0, 25, 'dstotijn/hetty', 'Hetty is an HTTP toolkit for security research.', ARRAY['dstotijn', 'holiman'], '2022-02-24T21:06:05.080468+00:00'),
