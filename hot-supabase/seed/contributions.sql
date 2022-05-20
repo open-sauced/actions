fd4170e6b3b71efd3d7c518cbf9bcf3bd705e300
0-vortex <0-vortex@users.noreply.github.com>
2022-05-20 12:57:44 +0000

    chore: seed supabase data [skip ci]

--- hot-supabase/seed/contributions.sql
+++ hot-supabase/seed/contributions.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: contributions; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-20T10:46:41.223Z
+-- Data for Name: contributions; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-20T12:57:43.687Z
 -- COPY contributions(id, repo_id, count, last_merged_at, contributor, url) FROM 'contributions.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
@@ -2725,10 +2725,8 @@ INSERT INTO contributions(id, repo_id, count, last_merged_at, contributor, url) 
 (3022, 100060912, 4, '2022-05-17T14:56:04', 'j4james', 'https://github.com/microsoft/terminal/pull/13109'),
 (3023, 100060912, 4, '2022-05-06T19:55:59', 'zadjii-msft', 'https://github.com/microsoft/terminal/pull/13044'),
 (3024, 100060912, 4, '2019-07-25T17:44:59', 'metathinker', 'https://github.com/microsoft/terminal/pull/2050'),
-(3025, 100060912, 4, '2021-10-12T18:00:06', 'mpela81', 'https://github.com/microsoft/terminal/pull/11485'),
 (3026, 100060912, 4, '2020-02-10T22:09:09', 'mkitzan', 'https://github.com/microsoft/terminal/pull/4309'),
 (3027, 100060912, 4, '2020-02-27T00:30:06', 'ZoeyR', 'https://github.com/microsoft/terminal/pull/4733'),
-(3028, 100060912, 4, '2020-01-14T17:07:07', 'mcpiroman', 'https://github.com/microsoft/terminal/pull/4190'),
 (3029, 100060912, 4, '2022-05-04T20:17:20', 'PankajBhojwani', 'https://github.com/microsoft/terminal/pull/13032'),
 (3030, 100060912, 4, '2022-01-10T16:31:32', 'eltociear', 'https://github.com/microsoft/terminal/pull/12124'),
 (3031, 100060912, 4, '2022-03-24T17:54:12', 'cinnamon-msft', 'https://github.com/microsoft/terminal/pull/12737'),
@@ -2773,6 +2771,8 @@ INSERT INTO contributions(id, repo_id, count, last_merged_at, contributor, url) 
 (3070, 273512313, 2, '2022-01-11T19:33:00', 'vchrombie', 'https://github.com/chaoss/community-handbook/pull/27'),
 (3071, 290234921, 1, '2020-10-13T18:17:08', 'MartinSeeler', 'https://github.com/JoseDeFreitas/awesome-youtubers/pull/38'),
 (3072, 273512313, 4, '2021-03-23T16:35:35', 'GeorgLink', 'https://github.com/chaoss/community-handbook/pull/14'),
+(3028, 100060912, 4, '2020-01-14T17:07:07', 'mcpiroman', 'https://github.com/microsoft/terminal/pull/4190'),
+(3025, 100060912, 4, '2021-10-12T18:00:06', 'mpela81', 'https://github.com/microsoft/terminal/pull/11485'),
 (3073, 290234921, 1, '2020-10-22T14:13:58', 'ifdiego', 'https://github.com/JoseDeFreitas/awesome-youtubers/pull/55'),
 (3074, 290234921, 1, '2021-06-15T22:08:23', 'arnavjindal', 'https://github.com/JoseDeFreitas/awesome-youtubers/pull/73'),
 (3075, 290234921, 1, '2020-10-12T02:16:27', 'alexandander', 'https://github.com/JoseDeFreitas/awesome-youtubers/pull/37'),
@@ -3671,8 +3671,8 @@ INSERT INTO contributions(id, repo_id, count, last_merged_at, contributor, url) 
 (4002, 228911039, 1, '2020-06-21T20:16:19', 'koji', 'https://github.com/antonioru/beautiful-react-hooks/pull/92'),
 (4003, 228911039, 1, '2020-02-20T11:06:31', 'danpoq', 'https://github.com/antonioru/beautiful-react-hooks/pull/64'),
 (4004, 228911039, 1, '2020-01-20T16:51:48', 'hsxfjames', 'https://github.com/antonioru/beautiful-react-hooks/pull/50'),
-(4006, 228911039, 1, '2020-01-06T17:24:29', 'theashraf', 'https://github.com/antonioru/beautiful-react-hooks/pull/33'),
 (4005, 228911039, 1, '2020-09-11T12:46:43', 'DannyDelott', 'https://github.com/antonioru/beautiful-react-hooks/pull/156'),
+(4006, 228911039, 1, '2020-01-06T17:24:29', 'theashraf', 'https://github.com/antonioru/beautiful-react-hooks/pull/33'),
 (4007, 228911039, 1, '2021-03-06T18:12:25', 'alecf', 'https://github.com/antonioru/beautiful-react-hooks/pull/213'),
 (4008, 228911039, 1, '2020-12-02T15:29:43', 'endlessqt', 'https://github.com/antonioru/beautiful-react-hooks/pull/179'),
 (4009, 228911039, 1, '2020-01-02T23:48:16', 'avitorio', 'https://github.com/antonioru/beautiful-react-hooks/pull/23'),
--- hot-supabase/seed/users.sql
+++ hot-supabase/seed/users.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: users; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-20T10:46:41.188Z
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-20T12:57:42.972Z
 -- COPY users(id, open_issues, has_stars_data, is_private, is_open_sauced_member, created_at, login) FROM 'users.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
@@ -110,11 +110,8 @@ INSERT INTO users(id, open_issues, has_stars_data, is_private, is_open_sauced_me
 (29484849, 0, true, false, true, '2022-05-16T00:32:51.894086+00:00', 'Lamiiine'),
 (34112620, 1, true, false, true, '2022-05-16T00:32:53.348652+00:00', 'rperez2021'),
 (17413306, 0, true, false, true, '2022-05-16T04:41:10.459881+00:00', 'babually'),
-(9283692, 0, true, true, true, '2022-05-16T05:23:33.036711+00:00', 'mpmccauley'),
 (62261622, 0, true, false, true, '2022-05-16T05:53:12.748342+00:00', 'sosplush'),
-(22186029, 0, true, true, true, '2022-05-16T06:28:43.660068+00:00', 'BenjaminMichaelis'),
 (24553355, 2, true, false, true, '2022-05-16T07:22:15.519571+00:00', 'vtcaregorodtcev'),
-(5901851, 1, true, false, true, '2022-05-16T07:51:11.59179+00:00', 'jayteealao'),
 (20727421, 0, true, false, true, '2022-05-16T08:39:11.42517+00:00', 'ryan3738'),
 (2609511, 0, true, false, true, '2022-05-16T08:58:35.604423+00:00', 'hackintoshrao'),
 (55982424, 1, true, false, true, '2022-05-16T09:47:29.084998+00:00', 'mukeshgurpude'),
@@ -210,6 +207,8 @@ INSERT INTO users(id, open_issues, has_stars_data, is_private, is_open_sauced_me
 (1645628, 8, true, false, true, '2022-05-20T08:13:26.017836+00:00', 'mikachan'),
 (34839258, 0, true, false, true, '2022-05-16T02:43:22.231878+00:00', 'adwinang'),
 (88112004, 0, true, false, true, '2022-05-16T03:44:58.140929+00:00', 'SClay345'),
+(9283692, 0, true, true, true, '2022-05-16T05:23:33.036711+00:00', 'mpmccauley'),
+(5901851, 1, true, false, true, '2022-05-16T07:51:11.59179+00:00', 'jayteealao'),
 (60850993, 1, true, false, true, '2022-05-20T08:13:27.132881+00:00', 'vinosamari'),
 (38838958, 1, true, false, true, '2022-05-20T08:13:28.310505+00:00', 'frvs'),
 (70440141, 1, true, false, true, '2022-05-20T08:13:29.428958+00:00', 'jessiebelle'),
@@ -238,4 +237,5 @@ INSERT INTO users(id, open_issues, has_stars_data, is_private, is_open_sauced_me
 (4199845, 6, true, false, true, '2022-05-20T09:14:25.520532+00:00', 'Wachiwi'),
 (14043845, 10, true, false, true, '2022-05-20T09:14:26.437678+00:00', 'jasonericdavis'),
 (5713670, 15, true, false, true, '2022-05-20T09:14:27.556372+00:00', 'bdougie'),
-(3597738, 0, true, false, true, '2022-05-19T17:57:58.946724+00:00', 'AdamasFR');
+(3597738, 0, true, false, true, '2022-05-19T17:57:58.946724+00:00', 'AdamasFR'),
+(22186029, 0, true, true, true, '2022-05-16T06:28:43.660068+00:00', 'BenjaminMichaelis');
