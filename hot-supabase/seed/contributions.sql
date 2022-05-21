184a817430d46d10a85e1609c22f2169728e855d
0-vortex <0-vortex@users.noreply.github.com>
2022-05-21 22:45:15 +0000

    chore: seed supabase data [skip ci]

--- hot-supabase/seed/contributions.sql
+++ hot-supabase/seed/contributions.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: contributions; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-21T20:45:28.399Z
+-- Data for Name: contributions; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-21T22:45:14.042Z
 -- COPY contributions(id, repo_id, count, last_merged_at, contributor, url) FROM 'contributions.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
@@ -2055,8 +2055,6 @@ INSERT INTO contributions(id, repo_id, count, last_merged_at, contributor, url) 
 (2354, 36633370, 2, '2022-01-11T18:12:53', 'Unkn0wnCat', 'https://github.com/awesome-selfhosted/awesome-selfhosted/pull/2821'),
 (2355, 36633370, 4, '2022-04-11T14:34:38', 'nodiscc', 'https://github.com/awesome-selfhosted/awesome-selfhosted/pull/2966'),
 (2356, 36633370, 4, '2018-12-01T14:10:26', 'Kickball', 'https://github.com/awesome-selfhosted/awesome-selfhosted/pull/1579'),
-(2357, 36633370, 4, '2016-11-23T14:47:16', 'm1guelpf', 'https://github.com/awesome-selfhosted/awesome-selfhosted/pull/863'),
-(2358, 36633370, 4, '2015-07-31T19:49:34', 'jungle-boogie', 'https://github.com/awesome-selfhosted/awesome-selfhosted/pull/85'),
 (2359, 36633370, 4, '2021-03-23T18:45:53', 'jorgeegomez', 'https://github.com/awesome-selfhosted/awesome-selfhosted/pull/2464'),
 (2360, 36633370, 4, '2021-07-10T17:34:50', 'n8225', 'https://github.com/awesome-selfhosted/awesome-selfhosted/pull/2617'),
 (2361, 36633370, 4, '2020-07-19T09:23:29', 'JC5', 'https://github.com/awesome-selfhosted/awesome-selfhosted/pull/2161'),
@@ -2077,6 +2075,8 @@ INSERT INTO contributions(id, repo_id, count, last_merged_at, contributor, url) 
 (2376, 141058176, 1, '2018-10-30T20:48:39', 'aausch', 'https://github.com/skidding/jobs-done/pull/11'),
 (2377, 141058176, 1, '2018-10-30T23:23:18', 'Timothee', 'https://github.com/skidding/jobs-done/pull/13'),
 (2378, 386384568, 1, '2021-07-20T13:16:06', 'jeremyletang', 'https://github.com/sniptt-official/ots/pull/3'),
+(2357, 36633370, 4, '2016-11-23T14:47:16', 'm1guelpf', 'https://github.com/awesome-selfhosted/awesome-selfhosted/pull/863'),
+(2358, 36633370, 4, '2015-07-31T19:49:34', 'jungle-boogie', 'https://github.com/awesome-selfhosted/awesome-selfhosted/pull/85'),
 (2379, 386384568, 1, '2021-08-11T19:46:54', 'marchanlon', 'https://github.com/sniptt-official/ots/pull/24'),
 (2380, 141058176, 4, '2018-11-01T14:44:24', 'skidding', 'https://github.com/skidding/jobs-done/pull/16'),
 (2381, 14741294, 1, '2018-10-29T21:32:28', 'MauroCoppola', 'https://github.com/RefugeRestrooms/refugerestrooms/pull/519'),
@@ -4698,7 +4698,6 @@ INSERT INTO contributions(id, repo_id, count, last_merged_at, contributor, url) 
 (5030, 203587744, 4, '2019-11-30T13:26:00', 'hosseinnedaee', 'https://github.com/hoppscotch/hoppscotch/pull/380'),
 (5031, 203587744, 4, '2020-02-29T18:50:28', 'yubathom', 'https://github.com/hoppscotch/hoppscotch/pull/623'),
 (5032, 203587744, 1, '2019-11-27T11:52:31', 'athul', 'https://github.com/hoppscotch/hoppscotch/pull/344'),
-(5033, 203587744, 4, '2021-08-09T08:04:20', 's-r-x', 'https://github.com/hoppscotch/hoppscotch/pull/1769'),
 (5034, 203587744, 3, '2019-10-04T04:57:16', 'edisonaugusthy', 'https://github.com/hoppscotch/hoppscotch/pull/182'),
 (5035, 203587744, 4, '2020-05-15T15:57:20', 'sboulema', 'https://github.com/hoppscotch/hoppscotch/pull/878'),
 (5036, 203587744, 4, '2021-12-02T14:50:25', '0xc0Der', 'https://github.com/hoppscotch/hoppscotch/pull/1952'),
@@ -4707,8 +4706,8 @@ INSERT INTO contributions(id, repo_id, count, last_merged_at, contributor, url) 
 (5039, 203587744, 3, '2020-10-05T16:37:04', 'LeoMartinDev', 'https://github.com/hoppscotch/hoppscotch/pull/1237'),
 (5040, 203587744, 4, '2020-04-08T17:11:03', 'levrik', 'https://github.com/hoppscotch/hoppscotch/pull/748'),
 (5041, 203587744, 4, '2022-04-30T13:53:43', 'anwarulislam', 'https://github.com/hoppscotch/hoppscotch/pull/2299'),
-(5042, 56894212, 4, '2022-02-21T01:58:20', 'apangin', 'https://github.com/jvm-profiling-tools/async-profiler/pull/553'),
 (5043, 56894212, 4, '2017-09-27T10:20:09', 'goldshtn', 'https://github.com/jvm-profiling-tools/async-profiler/pull/53'),
+(5042, 56894212, 4, '2022-02-21T01:58:20', 'apangin', 'https://github.com/jvm-profiling-tools/async-profiler/pull/553'),
 (5044, 56894212, 1, '2017-10-16T20:44:25', 'nitsanw', 'https://github.com/jvm-profiling-tools/async-profiler/pull/57'),
 (5045, 56894212, 2, '2020-09-02T21:17:47', 'yuzawa-san', 'https://github.com/jvm-profiling-tools/async-profiler/pull/346'),
 (5046, 56894212, 2, '2022-02-13T17:34:44', 'ghaug', 'https://github.com/jvm-profiling-tools/async-profiler/pull/537'),
@@ -4900,6 +4899,7 @@ INSERT INTO contributions(id, repo_id, count, last_merged_at, contributor, url) 
 (5232, 9052236, 4, '2019-03-04T12:31:52', 'phupe', 'https://github.com/nextflow-io/nextflow/pull/1020'),
 (5233, 9052236, 1, '2018-07-10T14:30:37', 'rsuchecki', 'https://github.com/nextflow-io/nextflow/pull/787'),
 (5234, 9052236, 4, '2022-01-25T16:16:27', 'phue', 'https://github.com/nextflow-io/nextflow/pull/2590'),
+(5033, 203587744, 4, '2021-08-09T08:04:20', 's-r-x', 'https://github.com/hoppscotch/hoppscotch/pull/1769'),
 (5235, 9052236, 4, '2021-11-08T16:47:32', 'mes5k', 'https://github.com/nextflow-io/nextflow/pull/2430'),
 (5236, 9052236, 4, '2019-04-16T07:46:57', 'sven1103', 'https://github.com/nextflow-io/nextflow/pull/1113'),
 (5237, 9052236, 3, '2020-08-01T15:57:41', 'pachiras', 'https://github.com/nextflow-io/nextflow/pull/1615'),
--- hot-supabase/seed/users_to_repos_votes.sql
+++ hot-supabase/seed/users_to_repos_votes.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: users_to_repos_votes; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-21T20:45:28.110Z
+-- Data for Name: users_to_repos_votes; Type: TABLE DATA; Schema: public; Timestamp: 2022-05-21T22:45:13.947Z
 -- COPY users_to_repos_votes(id, user_id, repo_id, created_at) FROM 'users_to_repos_votes.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
