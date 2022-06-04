d040ab107b9fa8a944ecda8977fb0c5cb8ac8587
0-vortex <0-vortex@users.noreply.github.com>
2022-06-04 12:51:38 +0000

    chore: seed supabase data [skip ci]

--- hot-supabase/seed/contributions.sql
+++ hot-supabase/seed/contributions.sql
@@ -1,5 +1,5 @@
 --
--- Data for Name: contributions; Type: TABLE DATA; Schema: public; Timestamp: 2022-06-04T10:44:46.870Z
+-- Data for Name: contributions; Type: TABLE DATA; Schema: public; Timestamp: 2022-06-04T12:51:36.920Z
 -- COPY contributions(id, repo_id, count, last_merged_at, contributor, url) FROM 'contributions.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
