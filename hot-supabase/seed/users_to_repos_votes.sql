2275a6f5c1033c154393597edd3b8ec014712537
0-vortex <0-vortex@users.noreply.github.com>
2022-11-24 20:44:30 +0000

    chore: seed supabase data [skip ci]

--- hot-supabase/seed/users_to_repos_votes.sql
+++ hot-supabase/seed/users_to_repos_votes.sql
@@ -1,11 +1,11 @@
 --
--- Data for Name: users_to_repos_votes; Type: TABLE DATA; Schema: public; Timestamp: 2022-11-24T18:46:08.858Z
+-- Data for Name: users_to_repos_votes; Type: TABLE DATA; Schema: public; Timestamp: 2022-11-24T20:44:22.804Z
 -- COPY users_to_repos_votes(id, user_id, repo_id, created_at, updated_at, deleted_at) FROM 'users_to_repos_votes.csv' WITH DELIMITER ',' CSV HEADER;
 --
 
 INSERT INTO users_to_repos_votes(id, user_id, repo_id, created_at, updated_at, deleted_at) VALUES
-(209, 54102389, 426820139, '2022-09-05T03:04:10.418161+00:00', '2022-09-05T03:04:10.418161+00:00', null),
 (192, 5713670, 1062897, '2022-08-05T17:04:34.439048+00:00', '2022-08-05T17:04:34.439048+00:00', null),
+(209, 54102389, 426820139, '2022-09-05T03:04:10.418161+00:00', '2022-09-05T03:04:10.418161+00:00', null),
 (224, 89579421, 119811010, '2022-09-15T04:56:25.052895+00:00', '2022-09-15T04:56:25.052895+00:00', null),
 (239, 97243126, 126577260, '2022-09-29T03:42:00.540517+00:00', '2022-09-29T03:42:00.540517+00:00', null),
 (253, 37635083, 357728969, '2022-10-09T22:45:54.775585+00:00', '2022-10-09T22:45:54.775585+00:00', null),
